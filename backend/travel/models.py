35a939ed17e68eaaeed4c8348b675848c7096ddf
MaxK9999 <maxkoltypin@gmail.com>
2023-11-13 11:29:14 +0100

    deleted unnecessary User model

--- backend/travel/models.py
+++ backend/travel/models.py
@@ -1,27 +1,9 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(AbstractUser):
-    # Add a related_name attribute to avoid clashes
-    groups = models.ManyToManyField(
-        "auth.Group",
-        related_name="users",
-        related_query_name="user",
-        blank=True,
-    )
-    
-    user_permissions = models.ManyToManyField(
-        "auth.Permission",
-        related_name="users",
-        related_query_name="user",
-        blank=True,
-    )
-
-
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='blog_posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
