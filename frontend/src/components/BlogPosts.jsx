8a3e55844fa329e0faf3af981b75a0b3bbd11b62
MaxK9999 <maxkoltypin@gmail.com>
2023-11-15 13:21:12 +0100

    changed background, started working on Home page

--- frontend/src/components/BlogPosts.jsx
+++ frontend/src/components/BlogPosts.jsx
@@ -19,23 +19,41 @@ const BlogPosts = () => {
 
     }, []);
 
-    return (
-        <div className="blog-posts">
+    const heroSection = (
+        <div className="hero-section">
             <h1>Blog Posts</h1>
-            <ul>
-            {blogPosts.map((post) => (
-                <figure key={post.id} className="snip1577" >
-                    <img src={post.banner_image} alt={post.title} />
-                    <figcaption>
-                    <h3>{post.title}</h3><br/>
-                    <h4>{post.content}</h4>
-                    <br/>
-                    <h4>Created By: {post.author}</h4>
-                    </figcaption>
-                </figure>
-                ))}
-            </ul>
-        </div> 
+            <p>
+                Embark on a journey of discovery with our captivating blog posts. 
+                From hidden gems to must-visit destinations, find inspiration for your next adventure right here.
+                Or simply allow yourself some relaxation by checking out our curated collection of exotic destinations!
+
+                <br /><br />
+                Something specific you're looking for? Try your look with the search bar!
+            </p>
+
+        </div>
+    )
+
+
+    return (
+        <section className="blog-posts-page">
+            {heroSection}
+            <div className="blog-posts">
+                    <ul>
+                    {blogPosts.map((post) => (
+                        <figure key={post.id} className="snip1577" >
+                            <img src={post.banner_image} alt={post.title} loading="lazy" />
+                            <figcaption>
+                            <h3>{post.title}</h3><br/>
+                            <h4>{post.content}</h4>
+                            <br/>
+                            <h4>Created By: {post.author}</h4>
+                            </figcaption>
+                        </figure>
+                        ))}
+                    </ul> 
+            </div> 
+        </section>
     ); 
 };
  
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import "../components_styles/Home.css";
 
 const Home = () => {
-    return (
+    
+    const heroSectionHome = (
         <div className="home">
             <h1>Home</h1>
+            <p>
+                What do <strong>YOU</strong> define as home? <br />
+                We at OdysseyOpus believe that home is where the heart is.
+            </p>
         </div>
     )
-}
-    
+        
+    return (
+        <div>
+            {heroSectionHome}
+        </div>
+    )
 
-
+};
 export default Home
