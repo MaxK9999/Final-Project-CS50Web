6f2e41292643980e45c3bf9f25b49056a0f26512
Your Name <maxkoltypin@gmail.com>
2023-12-05 18:36:44 +0100

    fixed session logging in AuthContext

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,15 +1,29 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { useState, useContext, createContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+    // Check for the presence of session in local storage
+    useEffect(() => {
+        const session = localStorage.getItem("sessionid");
+
+        // If session exists, set isAuthenticated to true
+        if (session) {
+            setIsAuthenticated(true);
+        }
+    }, []);
+
     const login = () => {
+        // Set session in local storage
+        localStorage.setItem("sessionid", "sessionid");
         setIsAuthenticated(true);
     };
     
     const logout = () => {
+        // Remove session from local storage
+        localStorage.removeItem("sessionid");
         setIsAuthenticated(false);
     };
     
