2181b0ab4f43eba48fed2300a387d306620f7bb3
Your Name <maxkoltypin@gmail.com>
2023-12-05 19:25:29 +0100

    email form works

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -8,11 +8,16 @@ export const api = axios.create({
     baseURL: 'http://localhost:8000/api/',
 });
 
-// Fetch Blog Posts from the API
+function getCookie(name) {
+    const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+    return cookieValue ? cookieValue.pop() : '';
+};
+
+// Fetch Blog Posts from the BlogPost API
 export const getBlogPosts = async (searchQuery = '') => {
     try {
         const response = await api.get(`blogposts?search=${searchQuery}`);
-        console.log('API Request succesful', response);
+        console.log('Blog posts fetched succesful', response);
         return response;
     } catch (error) {
         console.log('Error fetching blog posts:', error);
@@ -20,4 +25,20 @@ export const getBlogPosts = async (searchQuery = '') => {
     }
 };
 
-// Fetch email data from the API
+// Fetch email data from the EmailHandler API
+export const getEmailData = async (data) => {
+    try {
+        const csrftoken = getCookie('csrftoken');
+        const response = await api.post('send_mail/', data, {
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            withCredentials: true
+        });
+        console.log('Email sent succesful', response);
+        return response;
+    } catch (error) {
+        console.log('Error sending email:', error);
+        throw error;
+    }
+};
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./Heading";
 import "../components_styles/Heading.css";
 import "../components_styles/Contact.css";
+import { getEmailData } from "../Api";
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        subject: "",
+        message: "",       
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await getEmailData(formData);
+            console.log("Email sent successfully:", response.data);
+            setFormData({
+                subject: "",
+                message: "",
+            });
+        } catch (error) {
+            console.error("Error sending email:", error);
+        }
+    };
+
+
     return (
         <section className="contact-page">
             <div className="heading">
                 <Heading title="Contact" />
+                <p>
+                    We'd love to hear from you! Please fill out the form below and we'll get back to you as soon as possible.
+                </p>
+            </div>
+            <div className="contact-form">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="subject">Subject:</label>
+                        <input
+                        type="text"
+                        id="subject"
+                        name="subject"
+                        value={formData.subject}
+                        onChange={handleChange}
+                        required
+                        />
+
+                    <label htmlFor="message">Message:</label>
+                        <textarea
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        required
+                        />
+
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </section>
     );
